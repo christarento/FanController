a24a49d762fd71981e9c7b3e53fc889b5967306a
Christo <christo@chrisalys.org>
2014-01-23 22:04:02 +0100

    solution / project creation

---
+++ desk_winder.c
@@ -0,0 +1,17 @@
+/*
+ * desk_winder.c
+ *
+ * Created: 23/01/2014 22:00:10
+ *  Author: Christo
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
